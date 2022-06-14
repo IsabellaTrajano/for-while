dca4b146c3d046b1d05b27297fc84fb51669d5e9
IsabellaTrajano <bellrdapct@gmail.com>
2022-06-14 14:27:57 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+for (let count = 1; count <= 10; count ++){
+    console.log(count)
+} 
+
+
+let count2 = 10
+
+while(count2 >= 1){
+    console.log(count2)
+    count2 -= 1
+} 
+
+
+for (let count = 1; count <100; count += 2)
+console.log(count) 
+
+
+let count3 = 1
+
+while(100 >= count3){
+    console.log(count3)
+    count3 += 2
+} 
+
+
+for (let count = 0; count <= 100; count += 2)
+console.log(count) 
+
+
+let count4 = 0
+
+while(100 >= count4){
+    console.log(count4)
+    count4 += 2
+} 
+
+
+
+
+
+
